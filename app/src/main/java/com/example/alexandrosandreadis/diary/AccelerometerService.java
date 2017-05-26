f7bfc11fc9a3a0ba97c8ee813ee7f9a4ab4cc2c2
Darkbreath89 <Darkbreath89@mail.com>
2017-05-26 20:05:07 +0300

    Version 1.5 #Almost

---
+++ app/src/main/java/com/example/alexandrosandreadis/diary/AccelerometerService.java
@@ -0,0 +1,68 @@
+package com.example.alexandrosandreadis.diary;
+
+import android.app.Service;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by darkb on 26/5/2017.
+ */
+
+public class AccelerometerService extends Service implements SensorEventListener {
+    private SensorManager sensorManager=null;
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
+    private static final int SHAKE_THRESHOLD = 150;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+    @Override
+        public void onCreate(){
+        super.onCreate();
+
+        sensorManager=(SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManager.registerListener(this,sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor mySensor = sensorEvent.sensor;
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
+            long curTime = System.currentTimeMillis();
+
+            if ((curTime - lastUpdate) > 100) {
+                long diffTime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+
+                float speed = Math.abs(x + y + z - last_x - last_y - last_z)/ diffTime * 10000;
+
+                if (speed > SHAKE_THRESHOLD) {
+
+                    super.stopSelf();
+                    android.os.Process.killProcess(android.os.Process.myPid());
+                }
+                last_x = x;
+                last_y = y;
+                last_z = z;
+            }
+        }
+    }
+    @Override
+    public void onAccuracyChanged(Sensor arg0, int arg1) {
+    }
+}
--- app/src/main/java/com/example/alexandrosandreadis/diary/Unlock.java
+++ app/src/main/java/com/example/alexandrosandreadis/diary/Unlock.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,6 +20,7 @@ public class Unlock extends AppCompatActivity {
     EditText passwordText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unlock);
         unlockButton =(Button) findViewById(R.id.unlockButton);
@@ -36,6 +38,7 @@ public class Unlock extends AppCompatActivity {
         SharedPreferences sharedPref=getSharedPreferences(pref, Context.MODE_PRIVATE);
         if(sharedPref.contains(pass)){
             if(passIn.equals( sharedPref.getString(pass,null))){
+                startActivity(new Intent(this,EntryList.class));
                 startService(new Intent(this,AccelerometerService.class));
                 super.finish();
             }else{
